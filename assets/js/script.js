be81de4645c4be7697474184e7becea1147d52e3
laurieftn <laurie.ftn@gmail.com>
2020-11-02 15:03:51 +0100

    Envoi de l'exercice 4

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+var lastName = prompt("Nom :");
+var firstName = prompt("Prénom :");
+var espace = ' ';
+var city = prompt("Ville :");
+
+alert('Bonjour ' + firstName + espace + lastName + '\nrésidant à ' + city);
